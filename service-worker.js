fc0509c132e986f1758846b59416f4680571bd67
emircannnt <emircantepecikk@gmail.com>
2026-02-12 17:15:29 +0300

    Harden Spotify controls for publish readiness

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "takt-v2";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    }),
+  );
+});
